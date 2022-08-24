77a31ef7216161a61a5f81c8bf24e4bbfa75901b
Nir Nicole <Niko426@gmail.com>
2022-08-24 20:37:37 +0300

    changed the posts array to a map

--- week2/Teweeter-data-model/logicModule.js
+++ week2/Teweeter-data-model/logicModule.js
@@ -1,74 +1,75 @@
+/*
+  Author: Nir Nicole
+  Date: 24/8/22
 
+  |M| V C architecture:
+  this is the Model module.
+  this script contains all of the page data,
+  driven by the Controller and used by the View(renderModule).
+*/
+const TweeterLogic = function () {
+  const _postMap = new Map()
 
-const TweeterLogic = function(){
+  // those will use the module(by its closure) to keep uniq identifires
+  // to each post, untill the progrem will end.
+  let _nextPostId = 1
+  let _nextCommentId = 1
 
-    const _posts = []
-    let _nextPostId = 1
-    let _nextCommentId = 1
+  const postIdMechanism = () => `p${_nextPostId++}`
+  const commentIdMechanism = () => `c${_nextCommentId++}`
 
-    const postIdMechanism = () => `p${_nextPostId++}`
-    const commentIdMechanism = () => `c${_nextCommentId++}`
+  const getPosts = () => {
+    const valuesArray = Array.from(_postMap.values())
+    return JSON.parse(JSON.stringify(valuesArray))
+  }
 
-    const getPosts = () => _posts
-    
-    const addPost = str => {
-        _posts.push( 
-            {
-                text: str,
-                id: postIdMechanism(),
-                comments: []
-        } 
-        )
+  const addPost = (str) => {
+    const generatedPostId = postIdMechanism()
+    _postMap.set(generatedPostId, createPost(str, generatedPostId))
+  }
+  const removePost = (pid) => (_postMap.has(pid) ? _postMap.delete(pid) : null)
+
+  const addComment = (pid, commentText) => {
+    if (_postMap.has(pid)) {
+      const postComments = _postMap.get(pid).comments
+      postComments.push(createComment(commentText, commentIdMechanism()))
     }
+  }
 
-    const removePost = pid => {
-        for(let index in _posts){
-            if(_posts[index].id===pid){
-                _posts.splice(index,1)
-                break
-            }
+  const removeComment = (pid, cid) => {
+    if (_postMap.has(pid)) {
+      const relevantPost = _postMap.get(pid)
+      for (let cIndex in relevantPost.comments) {
+        if (relevantPost.comments[cIndex].id === cid) {
+          relevantPost.comments.splice(cIndex, 1)
+          break
         }
+      }
     }
+  }
 
-    const addComment = (pid, commentText) => {
-        
-        for(let post of _posts){
-            if(post.id===pid){
-                post.comments.push( 
-                    {
-                        id: commentIdMechanism(),
-                        text: commentText,
-                    }
-                )
-                break
-            }
-        }
-    }
-    
-        
-    const removeComment = (pid, cid) => {
-        for(let index in _posts){
-            if(_posts[index].id===pid){
-                for(let cIndex in _posts[index].comments){
-                    if(_posts[index].comments[cIndex].id===cid){
-                        _posts[index].comments.splice(cIndex,1)
-                        break
-                    }
-                }
-                break
-            }
-        }
-    }
-
-
-    return {
-        getPosts: getPosts,
-        addPost: addPost,
-        removePost: removePost,
-        addComment: addComment,
-        removeComment: removeComment
-    }
+  return {
+    getPosts: getPosts,
+    addPost: addPost,
+    removePost: removePost,
+    addComment: addComment,
+    removeComment: removeComment,
+  }
 }
 
-
-
+/*
+    Utils:
+ */
+const createPost = (data, pid) => {
+  return {
+    text: data,
+    id: pid,
+    comments: [],
+  }
+}
+const createComment = (data, cid) => {
+  return {
+    id: cid,
+    text: data,
+  }
+}
