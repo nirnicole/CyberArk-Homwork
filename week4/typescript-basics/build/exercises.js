7cf3a5bfe2f8da7224cb6d66bd0f9225f630ff49
Nir Nicole <Niko426@gmail.com>
2022-09-04 15:09:00 +0300

    typescript added

---
+++ week4/typescript-basics/build/exercises.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(arr) {
+    let sumArr = 0;
+    arr.forEach(a => sumArr += a);
+    return sumArr;
+}
---
+++ week4/typescript-basics/src/exercises.ts
@@ -0,0 +1,5 @@
+function sum(arr: number[]): number {
+  let sumArr: number = 0
+  arr.forEach(a => sumArr+=a)
+  return sumArr
+}
